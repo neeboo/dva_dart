57a0f007d0bed134a999432a05d21be52000196c
neeboo <ghostcorn@gmail.com>
2019-01-27 21:00:52 +0800

    refine library 	modified:   .packages 	new file:   lib/dva_dart.dart 	deleted:    lib/src/dva_dart.dart 	modified:   pubspec.yaml

---
+++ lib/dva_dart.dart
@@ -0,0 +1,8 @@
+library dva_dart;
+
+export './src/Action.dart';
+export './src/Effect.dart';
+export './src/Model.dart';
+export './src/Reducer.dart';
+export './src/State.dart';
+export './src/Store.dart';
--- lib/src/dva_dart.dart
+++
@@ -1,8 +0,0 @@
-library dva_dart;
-
-export 'Action.dart';
-export 'Effect.dart';
-export 'Model.dart';
-export 'Reducer.dart';
-export 'State.dart';
-export 'Store.dart';
